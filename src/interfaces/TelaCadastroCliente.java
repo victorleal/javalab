64deb384f6d4080e398fc6303621d0fb6df395e5
Victor Leal <victorleal18@gmail.com>
2014-04-26 15:57:26 -0300

    Updating some interfaces

--- src/interfaces/TelaCadastroCliente.java
+++ src/interfaces/TelaCadastroCliente.java
@@ -1,58 +1,165 @@
 package interfaces;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaCadastroCliente extends JPanel {
-	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_2;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
+	private JTextField textField_12;
+	private JTextField textField_13;
 
 	/**
 	 * Create the panel.
 	 */
 	public TelaCadastroCliente() {
-		setLayout(new MigLayout("", "[15px][4px][55px,grow][4px][67px][4px][42px][4px][71px][4px][105px][96px]", "[1px][25px][][][][][][][]"));
+		setLayout(new MigLayout("", "[55.00,grow][114.00,grow][][pref!][][91.00,grow]", "[][][][][][][][][][][]"));
+		
+		JLabel lblCadastrarCliente = new JLabel("Cadastrar Cliente");
+		add(lblCadastrarCliente, "cell 0 0 6 1,alignx center");
 		
 		JLabel lblNome = new JLabel("Nome:");
-		add(lblNome, "cell 0 0,alignx left,aligny center");
+		add(lblNome, "cell 0 2,alignx right");
 		
 		textField = new JTextField();
-		add(textField, "cell 2 0 9 1,grow");
+		add(textField, "cell 1 2 5 1,growx");
 		textField.setColumns(10);
 		
 		JLabel lblCpf = new JLabel("CPF:");
-		add(lblCpf, "cell 0 1,alignx left,aligny center");
+		add(lblCpf, "cell 0 3,alignx right");
 		
 		textField_1 = new JTextField();
+		add(textField_1, "cell 1 3,growx");
 		textField_1.setColumns(10);
-		add(textField_1, "cell 2 1 9 1,growx");
 		
 		JLabel lblCelular = new JLabel("Celular:");
-		add(lblCelular, "cell 0 2,alignx left,aligny center");
+		add(lblCelular, "cell 2 3,alignx right");
+		
+		textField_4 = new JTextField();
+		add(textField_4, "cell 3 3,growx");
+		textField_4.setColumns(10);
 		
 		JLabel lblTelefone = new JLabel("Telefone:");
-		add(lblTelefone, "cell 0 3,alignx left,aligny center");
+		add(lblTelefone, "cell 4 3,alignx right");
+		
+		textField_5 = new JTextField();
+		add(textField_5, "cell 5 3,growx");
+		textField_5.setColumns(10);
 		
 		JLabel lblEmail = new JLabel("Email:");
-		add(lblEmail, "cell 0 4,alignx left,aligny center");
+		add(lblEmail, "cell 0 4,alignx trailing");
+		
+		textField_3 = new JTextField();
+		add(textField_3, "cell 1 4 5 1,growx");
+		textField_3.setColumns(10);
 		
 		JLabel lblEndereo = new JLabel("Endere\u00E7o:");
-		add(lblEndereo, "cell 0 5,alignx left,aligny center");
+		add(lblEndereo, "cell 0 5,alignx trailing");
 		
-		JButton btnCadastrar = new JButton("Cadastrar");
-		add(btnCadastrar, "cell 10 8,alignx center,aligny center");
+		textField_6 = new JTextField();
+		add(textField_6, "cell 1 5 3 1,growx");
+		textField_6.setColumns(10);
+		
+		JLabel lblNmero = new JLabel("Número:");
+		add(lblNmero, "cell 4 5,alignx trailing");
+		
+		textField_2 = new JTextField();
+		add(textField_2, "cell 5 5,growx");
+		textField_2.setColumns(10);
+		
+		JLabel lblComplemento = new JLabel("Complemento:");
+		add(lblComplemento, "cell 0 6,alignx trailing");
+		
+		textField_7 = new JTextField();
+		add(textField_7, "cell 1 6,growx");
+		textField_7.setColumns(10);
+		
+		JLabel lblBairro = new JLabel("Bairro:");
+		add(lblBairro, "cell 2 6,alignx trailing");
+		
+		textField_8 = new JTextField();
+		add(textField_8, "cell 3 6,growx");
+		textField_8.setColumns(10);
+		
+		JLabel lblCep = new JLabel("CEP:");
+		add(lblCep, "cell 4 6,alignx trailing");
+		
+		textField_9 = new JTextField();
+		add(textField_9, "cell 5 6,growx");
+		textField_9.setColumns(10);
+		
+		JLabel lblCidade = new JLabel("Cidade:");
+		add(lblCidade, "cell 0 7,alignx trailing");
+		
+		textField_12 = new JTextField();
+		add(textField_12, "cell 1 7,growx");
+		textField_12.setColumns(10);
+		
+		JLabel lblEstado = new JLabel("Estado:");
+		add(lblEstado, "cell 2 7,alignx trailing");
+		
+		textField_11 = new JTextField();
+		add(textField_11, "cell 3 7,growx");
+		textField_11.setColumns(10);
+		
+		JLabel lblPas = new JLabel("País:");
+		add(lblPas, "cell 4 7,alignx trailing");
+		
+		textField_10 = new JTextField();
+		add(textField_10, "cell 5 7,growx");
+		textField_10.setColumns(10);
+		
+		JLabel lblClienteFidelidade = new JLabel("Cliente Fidelidade?");
+		add(lblClienteFidelidade, "cell 0 8,alignx right");
+		
+		JComboBox comboBox = new JComboBox();
+		add(comboBox, "cell 1 8,growx");
+		
+		JLabel lblPrograma = new JLabel("Programa:");
+		add(lblPrograma, "cell 2 8,alignx right");
+		
+		JComboBox comboBox_1 = new JComboBox();
+		add(comboBox_1, "cell 3 8,growx");
+		
+		JLabel lblNmero_1 = new JLabel("Número:");
+		add(lblNmero_1, "cell 4 8,alignx right");
+		
+		textField_13 = new JTextField();
+		add(textField_13, "cell 5 8,growx");
+		textField_13.setColumns(10);
 		
 		JButton btnCancelar = new JButton("Cancelar");
-		add(btnCancelar, "cell 11 8,alignx center,aligny center");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaPrincipal.EscondePaineis();
+			}
+		});
+		add(btnCancelar, "flowx,cell 0 10 6 1,alignx right");
+		
+		JButton btnCadastrar = new JButton("Cadastrar");
+		add(btnCadastrar, "cell 0 10,alignx right");
+		
 
 	}
 
--- src/interfaces/TelaPrincipal.java
+++ src/interfaces/TelaPrincipal.java
@@ -1,9 +1,13 @@
 package interfaces;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -18,10 +22,25 @@ public class TelaPrincipal extends JFrame {
 	/**
 	 * 
 	 */
-
+	
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private static TelaPrincipal frame;
+	public JPanel contentPane;
+	static TelaPrincipal frame;
+	
+	final static TelaConsultaTransportadora tcontrans = new TelaConsultaTransportadora ();
+	final static TelaCadastroTransportadora tcadtrans = new TelaCadastroTransportadora ();
+	final static TelaConsultaPedido tconped = new TelaConsultaPedido();
+	final static TelaControle tcont= new TelaControle();
+	final static TelaConsultaCliente tconcli = new TelaConsultaCliente();
+	final static TelaCadastroProduto tcadprod = new TelaCadastroProduto();
+	final static TelaCadastroPedido tcadped = new TelaCadastroPedido();
+	final static TelaConsultaProduto tconprod = new TelaConsultaProduto();
+	final static TelaCadastroCliente tcadcli = new TelaCadastroCliente();
+	
+	final static TelaAlterarCliente taltcli = new TelaAlterarCliente();
+	final static TelaAlterarProduto taltprod = new TelaAlterarProduto();
+	final static TelaAlterarTransportadora talttrans = new TelaAlterarTransportadora();
+	
 
 	/**
 	 * Launch the application.
@@ -31,7 +50,7 @@ public class TelaPrincipal extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					frame = new TelaPrincipal("ByteForte - Sistema de Vendas");
+					frame = new TelaPrincipal("ByteForte - Sitema de Vendas");
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,7 +58,7 @@ public class TelaPrincipal extends JFrame {
 			}
 		});
 	}
-
+	
 	public static void setLookAndFeel() {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -60,85 +79,174 @@ public class TelaPrincipal extends JFrame {
 
 	/**
 	 * Create the frame.
-	 * 
-	 * @param titulo
+	 * @param titulo 
 	 */
 	public TelaPrincipal(String titulo) {
-
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent arg0) {
+				frame.setIconImage(new ImageIcon(getClass().getResource("computer.png")).getImage());
+			}
+		});
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 350);
 		setTitle(titulo);
-
+		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-
+		
 		JMenu mnArquivo = new JMenu("Arquivo");
 		menuBar.add(mnArquivo);
-
+		
 		JMenuItem mntmSair = new JMenuItem("Sair");
 		mntmSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
 			}
 		});
+		
+		JMenuItem mntmGerenciamento = new JMenuItem("Gerenciamento");
+		mntmGerenciamento.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EscondePaineis();
+				frame.getContentPane().add(tcont);
+				tcont.setVisible(true);
+				frame.validate();
+			}
+		});
+		mnArquivo.add(mntmGerenciamento);
 		mnArquivo.add(mntmSair);
-
+		
 		JMenu mnCliente = new JMenu("Cliente");
 		menuBar.add(mnCliente);
-
+		
 		JMenuItem mntmCadastrar = new JMenuItem("Cadastrar");
-		final TelaCadastroCliente tcc = new TelaCadastroCliente();
 		mntmCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame.getContentPane().add(tcc);
-				tcc.setVisible(true);
-				frame.revalidate();
-
+				EscondePaineis();
+				frame.getContentPane().add(tcadcli);
+				tcadcli.setVisible(true);
+				frame.validate();
+				
 			}
 		});
 		mnCliente.add(mntmCadastrar);
-
+		
 		JMenuItem mntmConsultar_3 = new JMenuItem("Consultar");
 		mntmConsultar_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
+				EscondePaineis();
+				frame.getContentPane().add(tconcli);
+				tconcli.setVisible(true);
+				frame.validate();
+				
 			}
 		});
 		mnCliente.add(mntmConsultar_3);
-
+		
 		JMenu mnProduto = new JMenu("Produto");
 		menuBar.add(mnProduto);
-
+	
 		JMenuItem mntmCadastrar_1 = new JMenuItem("Cadastrar");
+		mntmCadastrar_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				EscondePaineis();
+				frame.getContentPane().add(tcadprod);
+				tcadprod.setVisible(true);
+				frame.validate();
+				
+			}
+		});
 		mnProduto.add(mntmCadastrar_1);
-
+		
 		JMenuItem mntmConsultar_2 = new JMenuItem("Consultar");
+		mntmConsultar_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EscondePaineis();
+				frame.getContentPane().add(tconprod);
+				tconprod.setVisible(true);
+				frame.validate();
+			}
+		});
 		mnProduto.add(mntmConsultar_2);
-
+		
 		JMenu mnPedido = new JMenu("Pedido");
 		menuBar.add(mnPedido);
-
+		
 		JMenuItem mntmCadastrar_2 = new JMenuItem("Cadastrar");
+		mntmCadastrar_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EscondePaineis();
+				frame.getContentPane().add(tcadped);
+				tcadped.setVisible(true);
+				frame.validate();
+			}
+		});
 		mnPedido.add(mntmCadastrar_2);
-
+		
 		JMenuItem mntmConsultar_1 = new JMenuItem("Consultar");
+		mntmConsultar_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EscondePaineis();
+				frame.getContentPane().add(tconped);
+				tconped.setVisible(true);
+				frame.validate();
+			}
+		});
 		mnPedido.add(mntmConsultar_1);
-
+		
 		JMenu mnTransportadora = new JMenu("Transportadora");
 		menuBar.add(mnTransportadora);
-
+		
 		JMenuItem mntmCadastrar_3 = new JMenuItem("Cadastrar");
+		mntmCadastrar_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EscondePaineis();
+				frame.getContentPane().add(tcadtrans);
+				tconped.setVisible(true);
+				frame.validate();
+			}
+		});
 		mnTransportadora.add(mntmCadastrar_3);
-
+		
 		JMenuItem mntmConsultar = new JMenuItem("Consultar");
+		mntmConsultar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EscondePaineis();
+				frame.getContentPane().add(tcontrans);
+				tconped.setVisible(true);
+				frame.validate();
+			}
+		});
 		mnTransportadora.add(mntmConsultar);
-
+		
 		JMenu mnSobre = new JMenu("Sobre");
 		menuBar.add(mnSobre);
 		contentPane = new JPanel();
-		contentPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null,
-				null, null));
+		contentPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 	}
+	
+	public static void EscondePaineis() 
+	{
+		frame.remove(tcontrans);
+		frame.remove(tcadtrans);
+		frame.remove(tconped);
+		frame.remove(tcadped);
+		frame.remove(tcadprod);
+		frame.remove(tconprod);
+		frame.remove(tconcli);
+		frame.remove(tcadcli);
+		frame.remove(tcont);
+		frame.remove(taltcli);
+		frame.remove(talttrans);
+		frame.remove(taltprod);
+		
+		frame.repaint();
+	}
+	
+	
 
 }
